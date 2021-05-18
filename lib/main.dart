765317cad257dec0bd82a3617752f0b6c495e04f
damoon <damoon.darvishi@gmail.com>
2021-05-18 11:17:54 +0430

    Getter & else if

--- lib/main.dart
+++ lib/main.dart
@@ -16,29 +16,46 @@ class MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'what\'s your favorite color?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
     },
     {
       'questionText': 'what\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'answers': [
+        {'text': 'Rabbit', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
     },
     {
       'questionText': 'what\'s your favorite instructure?',
-      'answers': ['Damon', 'Amir', 'Mohamad', 'Saeed'],
+      'answers': [
+        {'text': 'Damon', 'score': 1},
+        {'text': 'Damon', 'score': 1},
+        {'text': 'Damon', 'score': 1},
+        {'text': 'Damon', 'score': 1},
+      ],
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
-    if (_questionIndex < _questions.length) {
-      print('we have more questions ! ');
-    }
+  void _answerQuestion(int score) {
+    _totalScore += score;
 
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('we have more questions ! ');
+    }
   }
 
   @override
@@ -54,7 +71,7 @@ class MyAppState extends State<MyApp> {
                 questions: _questions,
                 questionIndex: _questionIndex,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
